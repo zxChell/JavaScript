277f0c613b7b60796677b4c9d941db132d141b79
zxChell <144452384+zxChell@users.noreply.github.com>
2024-01-23 11:55:54 +0300

    Add files via upload

---
+++ num1.js
@@ -0,0 +1,139 @@
+// let x = prompt('Как вас зовут?', );
+// console.log(x);
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+// console.log(c , d)
+// c = 2 , d = 1
+
+// let a = 2;
+// let x = 2 + (a *= 2);
+// console.log(a, x)
+// a = 4 , x = 6
+
+// "" + 1 + 0 // 10
+// "" - 1 + 0 // -1
+// true + false // 1
+// 6 / "3" // 2
+// "2" * "3" // 6
+// 4 + 5 + "px" // 9px
+// "$" + 4 + 5 // 45$
+// "4" - 2 // 2
+// "4px" - 2 // nan
+// "  -9  " + 5 //  -9  5
+// "  -9  " - 5 // -14
+// null + 1 // 1
+// undefined + 1 // nan
+// " \t \n" - 2 // -2
+// console.log(x) // подстовлял переменные для проверки
+
+// let x = parseInt(prompt('введите первое число')); // ParseInt добавить надо было..
+// let y = parseInt(prompt('введите второе число')); .. потому что он считает что это str
+// console.log(x + y);
+
+// /// Работа с переменными
+
+// let num = 3;
+// alert(num);
+
+// let a = 10;
+// let b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+
+// let c = 15;
+// let d = 2;
+
+// result = c + d;
+// console.log(result);
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+
+// alert(a + b + c);
+
+// let a = 17;
+// let b = 10;
+
+// let c = a - b;
+// let d = c;
+
+// console.log(c);
+
+// /// Работа со строками
+
+// let str = 'Привет мир!';
+// alert(str)
+
+// let str1 = 'Привет, ';
+// let str2 = 'Мир!';
+
+// alert(str1 + str2);
+
+// let name = 'Arsenii';
+// console.log(`hello ${name}`); // просто как в python self так же почти, только подставляешь `${переменная}`
+
+// let age = 17;
+// console.log(`тебе ${age} лет!`);
+
+// /// Функция prompt
+
+// let name = prompt('Как тебя зовут?', );
+// alert(`Ваше имя ${name}`);
+
+// let x = prompt('Введите любое число!', );
+// alert(x * x);
+
+// /// Обращение к символом 
+
+// let str = 'abcde';
+// console.log(str[0] + str[2] + str[4])
+
+// let num = '12345';
+// console.log(num[0] * num[1] * num[2] * num[3] * num[4]);
+
+// /// Практика
+
+// let x = 1; // час
+// let y = 24; // день
+// let z = 720; // месяц
+
+// console.log(`В одном часе ${x * 60 * 60} сек, так же в одном дне ${y * 60 * 60} сек и в одном месяце ${z * 60 * 60} сек`)
+
+// let x = 10 // час
+// let y = 57 // мин
+// let z = 12 // сек
+
+// console.log(`${x}:${y}:${z}`);
+
+// let x = 12;
+// c = x * x
+// console.log(c)
+
+// var num = 47;
+// num = num /= 7;
+// num = num *= 18;
+// num = num += 10;
+// num = num -= 15;
+// alert(num);
+
+// var num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+
+
+
+
+
+
+
+
+
